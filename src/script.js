380cf179486a31bd2b015f545574f11d9c2f29ff
h12j21-star <jh06079@naver.com>
2022-11-25 04:18:29 +0900

    colorPicker추가

--- src/script.js
+++ src/script.js
@@ -1,9 +1,18 @@
 const Header = document.querySelector('.todoHeader');
 const colorPicker = Header.querySelector('.inpColor');
-const inpName = Header.querySelector('.inp_name');
+const inpName = Header.querySelector('.inpName');
 const pushNameBtn = Header.querySelector('.pushName')
 const introduce = Header.querySelector('.introduce');
+const introduceName = Header.querySelector('.introduce span')
 const todoMain = document.querySelector('.todoMain')
 const todoInput = todoMain.querySelector('.inpTodo');
 const pushTodoBtn = todoMain.querySelector('.todoPush');
 const todoList = todoMain.querySelector('.todList');
+
+
+const colorChange = (e) =>{
+  let pickColor = e.target.value;
+  todoMain.style.backgroundColor=pickColor;
+}
+colorPicker.addEventListener('input',colorChange);
+
