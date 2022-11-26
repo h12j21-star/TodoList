9ce8f425e87d9c703236594fcbc702d4c4f3aa5e
h12j21-star <jh06079@naver.com>
2022-11-26 16:37:24 +0900

    localStorage 구성 변경

--- src/script.js
+++ src/script.js
@@ -9,36 +9,39 @@ const todoInput = todoMain.querySelector('.inpTodo');
 const pushTodoBtn = todoMain.querySelector('.todoPush');
 const todoList = todoMain.querySelector('.todList');
 
-
 //할일 저장하기
-// {todolist : [todos:{todo:[todo1,todo2],state:false},color:white]
-
-const todoListItem=JSON.parse(localStorage.getItem('todoListItem')) || [{todos:{todo:[]},state:false,color:colorPicker.value},]
 
 
+const todoListItem=JSON.parse(localStorage.getItem('todoListItem')) || [{todos:[{todo:[{ToDo:'',state:false}]}]}]
+console.log(todoListItem)
 function setUp(){
   setStoreItem();
   render();
 }
-function getStoreItem(item){
-  const todoListItem=JSON.parse(localStorage.getItem('todoListItem')) || [{todos:{todo:[]},state:false,color:colorPicker.value}]
-  return todoListItem[0][item]
 
+function getStoreItem(item){
+  const todoListItem=JSON.parse(localStorage.getItem('todoListItem')) || [{todos:[{todo:[{ToDo:'',state:false}]}]}]
+  return todoListItem[0][item]
 }
+
 function render(){
   //NAME
-  let getUserName = localStorage.getItem('name')
+  let getUserName = localStorage.getItem('Name')
   introduce.textContent = getUserName ? introduce.textContent + getUserName : 'hello' 
-  let getColor = getStoreItem('color');
+  //COLOR
+  let getColor = localStorage.getItem('Color');
   todoMain.style.
   backgroundColor=getColor;
   colorPicker.value=getColor;
+  //TODO
+
+
 }
 
 function setStoreItem(){
   const storeName = () =>{
     let userName = inpName.value;
-    localStorage.setItem('name',userName);
+    localStorage.setItem('Name',userName);
     introduce.textContent="hello ";
     introduce.textContent = introduce.textContent + userName;
   }
@@ -46,18 +49,19 @@ function setStoreItem(){
     let pickColor = e.target.value;
     todoMain.style.
     backgroundColor=pickColor;
-    todoListItem[0].color=pickColor;
-    localStorage.setItem('todoListItem',JSON.stringify(todoListItem))
+    localStorage.setItem('Color',pickColor)
+  }
+  //1. 할일 입력
+  //2. 등록 버튼
+  //3. 할일이 li로 추가
+  //3-1.새로고침해도 목록이 사라지지 않음
+  //4. 새로고침해도 존재함
+  const storeTodo = () =>{
+ 
   }
   pushNameBtn.addEventListener('click',storeName);
   colorPicker.addEventListener('input',storeColor);
+  pushTodoBtn.addEventListener('click',storeTodo);
 }
-
-
-
-
-
-
-
 
 setUp();
