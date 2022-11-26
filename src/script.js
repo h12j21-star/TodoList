29f7d25c7f88dcdac420bdf30d8ee171ca68f822
h12j21-star <jh06079@naver.com>
2022-11-26 17:12:44 +0900

    할일 localstorage 추가

--- src/script.js
+++ src/script.js
@@ -10,18 +10,15 @@ const pushTodoBtn = todoMain.querySelector('.todoPush');
 const todoList = todoMain.querySelector('.todList');
 
 //할일 저장하기
+const todoListItem=JSON.parse(localStorage.getItem('todoListItem')) || [{Todos:[]}]
 
-
-const todoListItem=JSON.parse(localStorage.getItem('todoListItem')) || [{todos:[{todo:[{ToDo:'',state:false}]}]}]
-console.log(todoListItem)
 function setUp(){
   setStoreItem();
   render();
 }
 
-function getStoreItem(item){
-  const todoListItem=JSON.parse(localStorage.getItem('todoListItem')) || [{todos:[{todo:[{ToDo:'',state:false}]}]}]
-  return todoListItem[0][item]
+function getStoreItem(){
+  const todoListItem=JSON.parse(localStorage.getItem('todoListItem')) || [{Todos:[]}]
 }
 
 function render(){
@@ -34,7 +31,7 @@ function render(){
   backgroundColor=getColor;
   colorPicker.value=getColor;
   //TODO
-
+ let getTodo = localStorage.getItem('Todos')
 
 }
 
@@ -56,8 +53,18 @@ function setStoreItem(){
   //3. 할일이 li로 추가
   //3-1.새로고침해도 목록이 사라지지 않음
   //4. 새로고침해도 존재함
-  const storeTodo = () =>{
+  const storeTodo = (e) =>{
+    const todoList = todoMain.querySelector('.todoList');
+    const todoLi = document.createElement('li');
+    todoLi.textContent = todoInput.value;
+    todoList.append(todoLi);
+    todoListItem[0].Todos.push({ToDo:todoInput.value,state:false})
+    console.log( todoListItem[0])
+    localStorage.setItem('todoListItem',JSON.stringify(todoListItem))
  
+  }
+  const storeState = () =>{
+
   }
   pushNameBtn.addEventListener('click',storeName);
   colorPicker.addEventListener('input',storeColor);
